6c41002ec98b718295d8777da13a936422c2412b
Sergio Villa <sergiovilla242@gmail.com>
2020-06-22 22:46:13 -0500

    templates

--- Productos/urls.py
+++ Productos/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
+from .views import productoView
 
 urlpatterns = [
-
+     path('',productoView.as_view(),name='index'),
 ]
--- Productos/views.py
+++ Productos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import producto
 
 # Create your views here.
+
+class productoView(TemplateView):
+    template_name = "productos/index.html"
+    
+    def get_context_data(self, **kwargs):
+        kwargs['productos'] = producto.objects.all()
+        return kwargs
+
+
+
+
+
